6baa8c64451d1362df1e98bec4ae60b6c2daf7e3
Christian <jesus.chumpitaz@vallegrande.edu.pe>
2023-09-06 13:39:38 -0500

    add search of items

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,44 +3,59 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
   editableId: number | null = null;
   newTask = '';
+  resultados: any[] = [];
+  busqueda: string = '';
+  noHayResultados: boolean = false;
+
   tasks: any[] = [
     {
       title: 'Crear la lista de tareas',
-      completed: true
+      completed: true,
     },
 
     {
       title: 'Realizar la estructura HTML',
-      completed: true
+      completed: true,
     },
 
     {
       title: 'Desplegar el proyecto en la web',
-      completed: true
+      completed: true,
+    },
+  ];
+
+
+  searchTask() {
+    if (this.busqueda) {
+      this.resultados = this.tasks.filter(item => item.title.toLowerCase().includes(this.busqueda.toLowerCase()));
+      this.noHayResultados = this.resultados.length === 0; 
+    } else {
+      this.resultados = []; 
+      this.noHayResultados = false;
     }
-  ]
+  }
 
   addTask() {
     const task = {
       title: this.newTask,
       completed: false,
-    }
+    };
     this.tasks.push(task);
     this.newTask = '';
   }
 
-  updateTask(task: any , title:string){
+  updateTask(task: any, title: string) {
     const index = this.tasks.indexOf(task);
     const updateTask = {
       title,
-      completed: task.completed
-    }
-    this.tasks[index] = {...task, ...updateTask};
+      completed: task.completed,
+    };
+    this.tasks[index] = { ...task, ...updateTask };
   }
 
   deleteTask(task: any) {
@@ -55,8 +70,6 @@ export class TodoListComponent {
   stopEdit(task: any, title: string): void {
     this.editableId = null;
     this.updateTask(task, title);
-    
   }
-
 
 }
